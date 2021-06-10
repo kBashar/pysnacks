fde8914ac45f5d45d5a45f5958a25cffa1be86f6
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2021-06-10 19:35:36 +0600

    white loop bracks

--- main.py
+++ main.py
@@ -51,8 +51,6 @@ def draw_frog(index):
 
 def draw_sanke():
     global X_Tail_Cell, Y_Tail_Cell, snake_pos_list
-    if X_Cell>19 or X_Cell<0 or Y_Cell>19 or Y_Cell<0:
-        return False 
     for pos in snake_pos_list:
         x, y = get_cell_pos(pos)
         pygame.draw.rect(screen, Color_snake, pygame.Rect((Cell_size*x)+1, (Cell_size*y)+1, Cell_size-1, Cell_size-1))
@@ -61,28 +59,41 @@ def draw_sanke():
     pygame.display.flip()
     return True
 
+def check_game_over():
+    global X_Cell , Y_Cell, snake_pos_list, GAME_ON
+    index =get_index_from_xy(X_Cell, Y_Cell)
+    if index in snake_pos_list:
+        print("Game Over")
+        GAME_ON= False
+    elif (X_Cell>19 or X_Cell<0 or Y_Cell>19 or Y_Cell<0):
+        print("Game Over")
+        GAME_ON= False
+    else:
+        snake_pos_list.append(index)
+
 def snake_speed(direction):
     global X_Cell, Y_Cell, X_Tail_Cell, Y_Tail_Cell
     if len(snake_pos_list)>0:
         X_Tail_Cell, Y_Tail_Cell = get_cell_pos(snake_pos_list.pop(0))
     if direction == "down":
         Y_Cell += 1
-        snake_pos_list.append(get_index_from_xy(X_Cell, Y_Cell))
+        check_game_over()
         if not draw_sanke():
             Y_Cell -= 1
     elif direction == "up":
         Y_Cell -= 1
-        snake_pos_list.append(get_index_from_xy(X_Cell, Y_Cell))
+        check_game_over()
         if not draw_sanke():
             Y_Cell += 1
     elif direction == "right":
         X_Cell += 1
-        snake_pos_list.append(get_index_from_xy(X_Cell, Y_Cell))
+        check_game_over()
         if not draw_sanke():
             X_Cell -= 1
     elif direction == "left":
         X_Cell -= 1
-        snake_pos_list.append(get_index_from_xy(X_Cell, Y_Cell))
+        # snake_pos_list.append(get_index_from_xy(X_Cell, Y_Cell))
+        check_game_over()
         if not draw_sanke():
             X_Cell += 1
     snake_face = get_index_from_xy(X_Cell, Y_Cell)
@@ -93,7 +104,7 @@ def snake_speed(direction):
 
 
 def main():
-    global Y_Cell, X_Cell, SNAKE_FACE_DIRECTION, op_directions
+    global Y_Cell, X_Cell, SNAKE_FACE_DIRECTION, op_directions, GAME_ON
     screen.fill(Color_screen)
     for times in range(1, 20):
         changer = Cell_size * times
@@ -103,26 +114,27 @@ def main():
     draw_frog(generate_frog_pos())
     prev_direction = None
     while True:
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                sys.exit(0)
-            elif event.type == KEYDOWN:
-                key_pressed = pygame.key.name(event.key)
-                if key_pressed == "down":
-                    SNAKE_FACE_DIRECTION = "down"
-                elif key_pressed == "up":
-                    SNAKE_FACE_DIRECTION = "up"
-                elif key_pressed == "right":
-                    SNAKE_FACE_DIRECTION = "right"
-                elif key_pressed == "left":
-                    SNAKE_FACE_DIRECTION = "left"
-        if prev_direction==None:
-            prev_direction=SNAKE_FACE_DIRECTION
-        elif op_directions[prev_direction] != SNAKE_FACE_DIRECTION:
-            prev_direction= SNAKE_FACE_DIRECTION
-        else:
-            SNAKE_FACE_DIRECTION= prev_direction
-        snake_speed(SNAKE_FACE_DIRECTION)
-        print(snake_pos_list)
-        time.sleep(0.40)
+        if GAME_ON :
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    sys.exit(0)
+                elif event.type == KEYDOWN:
+                    key_pressed = pygame.key.name(event.key)
+                    if key_pressed == "down":
+                        SNAKE_FACE_DIRECTION = "down"
+                    elif key_pressed == "up":
+                        SNAKE_FACE_DIRECTION = "up"
+                    elif key_pressed == "right":
+                        SNAKE_FACE_DIRECTION = "right"
+                    elif key_pressed == "left":
+                        SNAKE_FACE_DIRECTION = "left"
+            if prev_direction==None:
+                prev_direction=SNAKE_FACE_DIRECTION
+            elif op_directions[prev_direction] != SNAKE_FACE_DIRECTION:
+                prev_direction= SNAKE_FACE_DIRECTION
+            else:
+                SNAKE_FACE_DIRECTION= prev_direction
+            snake_speed(SNAKE_FACE_DIRECTION)
+            print(snake_pos_list)
+            time.sleep(0.40)      
 main()
